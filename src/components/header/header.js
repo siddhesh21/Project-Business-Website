6cc8e6794aa3a4f10efce8ee7cecff1038c28e76
Siddhesh Kankekar <kankekas@tcd.ie>
2021-08-08 22:56:36 +0100

    Completed Header section + Nav Links setup

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,9 +9,30 @@ import menuItems from "./header.data";
 
 export default function Header({ className }) {
   return (
-    <header sxx={styles.header} className={className} id="header">
+    <header sx={styles.header} className={className} id="header">
       <Container sx={styles.container}>
         <Logo src={LogoDark} />
+        <Flex as="nav" sx={styles.nav}>
+          {menuItems.map((menuItem, i) => (
+            <Link
+              activeClass="active"
+              to={menuItem.path}
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+              key={i}
+            ></Link>
+          ))}
+        </Flex>
+        <Button
+          className="donate__btn"
+          variant="secondary"
+          aria-label="Get Started"
+        >
+          Get Started
+        </Button>
+        <MobileDrawer />
       </Container>
     </header>
   );
