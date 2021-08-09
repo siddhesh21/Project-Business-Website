8a1ea6fa739f0ed7715d2554d9ae21bbf9539343
Siddhesh Kankekar <kankekas@tcd.ie>
2021-08-09 14:05:08 +0100

    added Key-Feature-Selection card component

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -1,8 +1,50 @@
-import React from 'react';
-import { Box, Text, Heading } from 'theme-ui';
+import React from "react";
+import { Box, Text, Heading } from "theme-ui";
 
 export default function SectionHeader({ title, slogan, isWhite }) {
   return (
-    <h1>SectionHeader</h1>
+    <Box sx={{ variant: "sectionHeader" }}>
+      <Text
+        as="p"
+        sx={{
+          variant: "sectionHeader.subTitle",
+          color: isWhite ? "white" : "primary",
+          opacity: isWhite ? 0.7 : 1,
+        }}
+      >
+        {slogan}
+      </Text>
+      <Heading
+        as="h2"
+        sx={{
+          variant: "sectionHeader.title",
+          color: isWhite ? "white" : "heading",
+        }}
+      >
+        {title}
+      </Heading>
+    </Box>
   );
+}
+
+{
+  /* <Box sx={{ variant: "sectionHeader" }}>
+<Text
+  as="p"
+  sx={{
+    variant: "sectionHeader.subTitle",
+    color: "white",
+  }}
+></Text>
+</Box>
+
+<Box sx={{ variant: "sectionHeader" }}>
+      <Text
+        as="p"
+        sx={{
+          variant: "sectionHeader.subTitle",
+          color: "black",
+        }}
+      ></Text>
+    </Box> */
 }
